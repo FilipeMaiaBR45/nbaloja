7ec9d149f69d7e3223088b8e3d5247ddf8c15c8c
Filipe Maia <filopemaia2001@gmail.com>
2021-07-04 03:58:38 -0300

    Initial commit

---
+++ src/main/java/ufrn/tads/eaj/filipe/nbaloja/NbalojaApplication.java
@@ -0,0 +1,13 @@
+package ufrn.tads.eaj.filipe.nbaloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbalojaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbalojaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ufrn/tads/eaj/filipe/nbaloja/NbalojaApplicationTests.java
@@ -0,0 +1,13 @@
+package ufrn.tads.eaj.filipe.nbaloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbalojaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
